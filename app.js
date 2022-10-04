f884fae73ce9f46d1fe8251cb39f8630672a9816
yohcho <95256337+yohcho@users.noreply.github.com>
2022-10-04 04:17:13 -0400

    dlone

--- app.js
+++ app.js
@@ -29,9 +29,8 @@ app.use('/api/groups/', require('./routes/groupsRoutes'));
 app.use('/api/meetings/', require('./routes/meetingsRoutes'));
 app.use('/api/users/', require('./routes/usersRoutes'));
 
-app.use(express.static(path.resolve(__dirname, "./client/build")))
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
-})
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+}
 
 app.listen(process.env.PORT);
